2dac30299a635d100fa988b4e9ef2b9bbbc15a67
Md Maruf Hossain <ryanmaruf615@gmail.com>
2025-10-07 01:48:44 +0200

    checked the folder existed or not

--- Downloads_Management/FileOrganizer.java
+++ Downloads_Management/FileOrganizer.java
@@ -1,10 +1,27 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class FileOrganizer {
 
 
-    public void createFolders(HashMap<String,Integer> count ,String folderNames, String directoryPath){
+    public void createFolders(HashMap<String,Integer> count ,List<String> folderNames, String directoryPath){
+
+
+        //checking the folder is exsisted or not
+        for(String fName : folderNames)
+        {
+             Path folderPath = Paths.get(directoryPath, fName);
+            if (Files.exists(folderPath) && Files.isDirectory(folderPath)) {
+                System.out.println("Folder " + fName + " already exists.");
+            } else {
+                System.out.println("Folder " + fName + " does not exist."); 
+            }
+        }
+
 
         for (Map.Entry<String, Integer> entry : count.entrySet())
         {
