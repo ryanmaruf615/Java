5e3d9017b04f1ad8ed97568f407d865087a1cb14
Md Maruf Hossain <ryanmaruf615@gmail.com>
2026-02-28 00:34:34 +0100

    learning springboot jsp

--- SpringBootWebProject/src/main/java/org/learnmaruf/springbootwebproject/HomeController.java
+++ SpringBootWebProject/src/main/java/org/learnmaruf/springbootwebproject/HomeController.java
@@ -1,5 +1,6 @@
 package org.learnmaruf.springbootwebproject;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +10,14 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest request) {
+       int num1 = Integer.parseInt(request.getParameter("num1"));
+       int num2 = Integer.parseInt(request.getParameter("num2"));
+       System.out.println(num1+num2);
+
+        return "result.jsp";
     }
 }
