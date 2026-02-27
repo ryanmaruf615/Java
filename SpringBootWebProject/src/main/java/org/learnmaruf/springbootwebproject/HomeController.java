fa3940f6aa3980bb5852c37ae0352f3ce01adf34
Md Maruf Hossain <ryanmaruf615@gmail.com>
2026-02-28 00:49:05 +0100

    learning springboot jsp

--- SpringBootWebProject/src/main/java/org/learnmaruf/springbootwebproject/HomeController.java
+++ SpringBootWebProject/src/main/java/org/learnmaruf/springbootwebproject/HomeController.java
@@ -1,6 +1,7 @@
 package org.learnmaruf.springbootwebproject;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +14,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest request) {
+    public String add(HttpServletRequest request , HttpSession session) {
        int num1 = Integer.parseInt(request.getParameter("num1"));
        int num2 = Integer.parseInt(request.getParameter("num2"));
        System.out.println(num1+num2);
+
+      int result = num1 + num2;
+      session.setAttribute("result", result);
 
         return "result.jsp";
     }
--- SpringBootWebProject/src/main/webApp/result.jsp
+++ SpringBootWebProject/src/main/webApp/result.jsp
@@ -4,7 +4,8 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 </body>
-<h2>Result is :</h2>
+<h2>Result is : <%= session.getAttribute("result") %> </h2>
+
 
 </body>
 </html>
